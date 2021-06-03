683d917471df62e34534cb04b19b09009b4aee02
Abdul Shaik <AbdulNabi.Shaik26@T-Mobile.com>
2021-06-03 12:59:10 +0530

    my bookings styles and accessibility

--- src/app/my-bookings/my-bookings-routing.module.ts
+++ src/app/my-bookings/my-bookings-routing.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -11,7 +12,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
   declarations: [MyBookingsComponent],
 })
-export class MyBookingsRoutingModule {}
+export class MyBookingsRoutingModule { }
--- src/app/my-bookings/my-bookings.component.ts
+++ src/app/my-bookings/my-bookings.component.ts
@@ -6,8 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-bookings.component.scss']
 })
 export class MyBookingsComponent implements OnInit {
-
   constructor() { }
+
+  bookings = [
+    {
+      hotelName: 'Hotel Vistara',
+      location: 'Hyderabad',
+      guests: 4,
+      chekinTime: '28th May 2021 11 AM',
+      chekoutTime: '29th May 2021 11 AM',
+    },
+    {
+      hotelName: 'Hotel Novotel',
+      location: 'Vizag',
+      guests: 2,
+      chekinTime: '30th May 2021 11 AM',
+      chekoutTime: '31st May 2021 11 AM',
+    },
+  ];
 
   ngOnInit(): void {
   }
